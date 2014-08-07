f96aee2049a02fdd82d2d825cfab34e72029d5f4
Roland Sharp <rolandnsharp@gmail.com>
2014-08-07 16:49:05 +1000

    extract defer to a function

--- kgo.js
+++ kgo.js
@@ -2,6 +2,20 @@ var run = require('./run'),
     EventEmitter = require('events').EventEmitter,
     fnRegex = /^function.*?\((.*?)\)/;
 
+function defer(callback){
+    if (typeof setImmediate === "function"){
+        // In IE10, Node.js 0.9+, or https://github.com/NobleJS/setImmediate
+        setImmediate(function(){
+            callback();
+        });
+        
+    } else {
+        setTimeout(function(){
+            callback();
+        },0);
+    }
+}    
+
 function newKgo(){
     var returnlessId = 0,
         tasks = {},
@@ -39,17 +53,9 @@ function newKgo(){
 
     kgoFn.apply(null, arguments);
 
-    if (typeof setImmediate === "function"){
-        // In IE10, Node.js 0.9+, or https://github.com/NobleJS/setImmediate
-        setImmediate(function(){
-            run(tasks, results, kgoFn);
-        });
-
-    } else {
-        setTimeout(function(){
-            run(tasks, results, kgoFn);
-        },0);
-    }
+    defer(function(){
+        run(tasks, results, kgoFn);
+    });
 
     return kgoFn;
 }
