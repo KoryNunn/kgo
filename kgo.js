e84319e7e91bffa68f430d43d9c663f8d6d1271a
Roland Sharp <rolandnsharp@gmail.com>
2014-08-07 14:52:24 +1000

    if setImmediate is supported, use setImmediate instead of setTimeout

--- kgo.js
+++ kgo.js
@@ -2,25 +2,25 @@ var run = require('./run'),
     EventEmitter = require('events').EventEmitter,
     fnRegex = /^function.*?\((.*?)\)/;
 
-function newKgo(){
+function newKgo() {
     var returnlessId = 0,
         tasks = {},
         results = {},
         errorHandlers = {};
 
-    function kgoFn(name, dependencies, fn){
-        if(typeof name !== 'string'){
+    function kgoFn(name, dependencies, fn) {
+        if (typeof name !== 'string') {
             fn = dependencies;
             dependencies = name;
             name = (returnlessId++).toString() + '__returnless';
         }
 
-        if(typeof dependencies === 'function'){
+        if (typeof dependencies === 'function') {
             fn = dependencies;
             dependencies = [];
         }
 
-        if(typeof fn !== 'function'){
+        if (typeof fn !== 'function') {
             throw new Error('No function provided for task number ' + Object.keys(tasks).length + ' (' + name + ')');
         }
 
@@ -33,15 +33,25 @@ function newKgo(){
         return kgoFn;
     }
 
-    for(var key in EventEmitter.prototype){
+    for (var key in EventEmitter.prototype) {
         kgoFn[key] = EventEmitter.prototype[key];
     }
 
     kgoFn.apply(null, arguments);
 
-    setTimeout(function(){
-        run(tasks, results, kgoFn);
-    },0);
+    if (typeof setImmediate === "function") {
+        // In IE10, Node.js 0.9+, or https://github.com/NobleJS/setImmediate
+        setImmediate(function() {
+            run(tasks, results, kgoFn);
+        });
+        
+    } else {
+
+        setTimeout(function() {
+            run(tasks, results, kgoFn);
+        }, 0);
+
+    }
 
     return kgoFn;
 }
